4d00f1ebae07323534e2fd77ddfa1ce85055bc16
Subhrojit Hore <subhrojithore35@gmail.com>
2025-10-18 12:57:27 +0530

    Added Express Validators

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,35 @@
 const express = require("express");
-const User = require('../models/User')
+const User = require("../models/User");
 const router = express.Router();
-
+const { body, validationResult } = require("express-validator");
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post("/", (req, res) => {
-  try {
-    console.log(req.body);
-    const user = new User (req.body)
-    user.save()
-    res.send(req.body);
-  } catch (error) {
-    res.send({
-      error: `Ssup y'all? The ${error} shit is not buzzing, no cap.`,
-    });
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name!").isLength({ min: 3 }),
+    body("email", "Enter a valid email!").isEmail(),
+    body("password", "Password must be atleast 8 characters").isLength({
+      min: 8,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch(() =>
+        res
+          .status(500)
+          .json({ error: "User is already registered with this email!" })
+      );
   }
-});
+);
 
 module.exports = router;
