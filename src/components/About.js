ca5fc7f9992d6042fcb02d424081d6af4d9da611
Subhrojit Hore <subhrojithore35@gmail.com>
2025-10-23 00:35:46 +0530

    Performed setup of ContextAPI

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 
 const About = () => {
-  return <div>This is About.</div>;
+  return <div>This is about page.</div>;
 };
 
 export default About;
